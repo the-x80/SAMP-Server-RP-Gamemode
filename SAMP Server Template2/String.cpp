5677ca0961fcfe0901ae5190aee2f643a3390320
the-x80 <76942844+the-x80@users.noreply.github.com>
2021-08-23 14:12:08 +0200

    Updated String object Added + operator Added += operator

--- SAMP Server Template2/String.cpp
+++ SAMP Server Template2/String.cpp
@@ -1,6 +1,8 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <string>
 #include <Windows.h>
+
+#include "Framework.h"
 #include "String.h"
 
 String::String()
@@ -13,30 +15,188 @@ String::String()
 
 String::String(char* cstr_text)
 {
-#ifdef STRING_DEBUG
-	char cstr_DebugMessage[512];
-	sprintf(cstr_DebugMessage, "%s(char* cstr_text=%s) started.\n", __func__, cstr_text);
-	OutputDebugString(cstr_DebugMessage);
-#endif
+	if (cstr_text == nullptr) {
+		throw new Exceptions::ArgumentNullException();
+	}
 	this->cstr_Text = new char[strlen(cstr_text) + 1];
 	memset(this->cstr_Text, 0, (strlen(cstr_text) + 1) * sizeof(char));
 	strcpy(this->cstr_Text, cstr_text);
 	this->n_Length = strlen(cstr_text);
 	this->n_Size = (this->n_Length + 1) * sizeof(char);
-#ifdef STRING_DEBUG
-	sprintf(cstr_DebugMessage, "%s:Done\n.String::cstr_Text = %s\n", __func__, this->cstr_Text);
-	OutputDebugString(cstr_DebugMessage);
-#endif
 }
 
 String::~String()
 {
-	delete[] this->cstr_Text;
+	if (this->cstr_Text != nullptr) {
+		delete[] this->cstr_Text;
+	}
 	this->cstr_Text = nullptr;
 
 	this->n_Length = 0;
 	this->n_Size = 0;
 }
+
+
+/*
+String& String::operator=(String other)
+{
+	// TODO: String& String::operator=::Optimize it a bit.
+	int n_ResultLength = 64;
+	char* cstr_ResultText = nullptr;
+	try {
+		cstr_ResultText = new char[n_ResultLength];
+		memset(cstr_ResultText, 0, n_ResultLength);
+		sprintf(cstr_ResultText, "%s", other.cstr_Text);
+		delete[] this->cstr_Text;
+		this->cstr_Text = new char[n_ResultLength];
+		memcpy(this->cstr_Text, cstr_ResultText, n_ResultLength);
+		delete[] cstr_ResultText;
+	}
+	catch(std::bad_alloc e){
+	}
+	catch (std::exception e) {
+
+	}
+	if (cstr_ResultText == nullptr) {
+	}
+	
+	return *this;
+}
+
+String& String::operator=(short other)
+{
+	int n_ResultLength = (64 + 1) * sizeof(char);
+	char* cstr_ResultText = new char[n_ResultLength];
+	memset(cstr_ResultText, 0, n_ResultLength);
+	sprintf(cstr_ResultText, "%d", other);
+	delete[] this->cstr_Text;
+	this->cstr_Text = new char[n_ResultLength];
+	memcpy(this->cstr_Text, cstr_ResultText, n_ResultLength);
+	delete[] cstr_ResultText;
+	return *this;
+}
+
+String& String::operator=(int other)
+{
+	int n_ResultLength = (64 + 1) * sizeof(char);
+	char* cstr_ResultText = new char[n_ResultLength];
+	memset(cstr_ResultText, 0, n_ResultLength);
+	sprintf(cstr_ResultText, "%d", other);
+	delete[] this->cstr_Text;
+	this->cstr_Text = new char[n_ResultLength];
+	memcpy(this->cstr_Text, cstr_ResultText, n_ResultLength);
+	delete[] cstr_ResultText;
+	return *this;
+}
+
+String& String::operator=(long other)
+{
+	int n_ResultLength = (64 + 1) * sizeof(char);
+	char* cstr_ResultText = new char[n_ResultLength];
+	memset(cstr_ResultText, 0, n_ResultLength);
+	sprintf(cstr_ResultText, "%d", other);
+	delete[] this->cstr_Text;
+	this->cstr_Text = new char[n_ResultLength];
+	memcpy(this->cstr_Text, cstr_ResultText, n_ResultLength);
+	delete[] cstr_ResultText;
+	return *this;
+}
+
+String& String::operator=(float other)
+{
+	int n_ResultLength = (64 + 1) * sizeof(char);
+	char* cstr_ResultText = new char[n_ResultLength];
+	memset(cstr_ResultText, 0, n_ResultLength);
+	sprintf(cstr_ResultText, "%f", other);
+	delete[] this->cstr_Text;
+	this->cstr_Text = new char[n_ResultLength];
+	memcpy(this->cstr_Text, cstr_ResultText, n_ResultLength);
+	delete[] cstr_ResultText;
+	return *this;
+}
+
+String& String::operator=(double other)
+{
+	int n_ResultLength = (64 + 1) * sizeof(char);
+	char* cstr_ResultText = new char[n_ResultLength];
+	memset(cstr_ResultText, 0, n_ResultLength);
+	sprintf(cstr_ResultText, "%f", other);
+	delete[] this->cstr_Text;
+	this->cstr_Text = new char[n_ResultLength];
+	memcpy(this->cstr_Text, cstr_ResultText, n_ResultLength);
+	delete[] cstr_ResultText;
+	return *this;
+}
+
+String& String::operator=(char other)
+{
+	int n_ResultLength = (64 + 1) * sizeof(char);
+	char* cstr_ResultText = new char[n_ResultLength];
+	memset(cstr_ResultText, 0, n_ResultLength);
+	sprintf(cstr_ResultText, "%c", other);
+	delete[] this->cstr_Text;
+	this->cstr_Text = new char[n_ResultLength];
+	memcpy(this->cstr_Text, cstr_ResultText, n_ResultLength);
+	delete[] cstr_ResultText;
+	return *this;
+}
+
+String& String::operator=(char* other)
+{
+	if (other == nullptr) {
+		throw new Exceptions::NullPointerException();
+	}
+	int n_ResultLength = (strlen(other) + 1) * sizeof(char);
+	char* cstr_ResultText = new char[n_ResultLength];
+	memset(cstr_ResultText, 0, n_ResultLength);
+	sprintf(cstr_ResultText, "%s", other);
+	delete[] this->cstr_Text;
+	this->cstr_Text = new char[n_ResultLength];
+	memcpy(this->cstr_Text, cstr_ResultText, n_ResultLength);
+	delete[] cstr_ResultText;
+	return *this;
+}
+
+String& String::operator=(const char* other)
+{
+	if (other == nullptr) {
+		throw new Exceptions::NullPointerException();
+	}
+	int n_ResultLength = (strlen(other) + 1) * sizeof(char);
+	char* cstr_ResultText = new char[n_ResultLength];
+	memset(cstr_ResultText, 0, n_ResultLength);
+	sprintf(cstr_ResultText, "%s", other);
+	delete[] this->cstr_Text;
+	this->cstr_Text = new char[n_ResultLength];
+	memcpy(this->cstr_Text, cstr_ResultText, n_ResultLength);
+	delete[] cstr_ResultText;
+	return *this;
+}
+
+String& String::operator=(unsigned short other)
+{
+	// TODO: insert return statement here
+	return *this;
+}
+
+String& String::operator=(unsigned int other)
+{
+	// TODO: insert return statement here
+	return *this;
+}
+
+String& String::operator=(unsigned long other)
+{
+	// TODO: insert return statement here
+	return *this;
+}
+
+String& String::operator=(unsigned char other)
+{
+	// TODO: insert return statement here
+	return *this;
+}
+*/
 
 String& String::operator+(String other)
 {
@@ -51,23 +211,10 @@ String& String::operator+(String other)
 
 String& String::operator+(short other)
 {
-#ifdef STRING_DEBUG
-	char cstr_DebugMessage[512];
-	sprintf(cstr_DebugMessage, "%s started.\n", __func__);
-	OutputDebugString(cstr_DebugMessage);
-#endif
 	int n_ResultLength = (strlen(this->cstr_Text) + 64 + 1) * sizeof(char);
-#ifdef STRING_DEBUG
-	sprintf(cstr_DebugMessage, "%s:n_ResultLength = %d\n", __func__, n_ResultLength);
-	OutputDebugString(cstr_DebugMessage);
-#endif
 	char* cstr_ResultText = new char[n_ResultLength];
 	memset(cstr_ResultText, 0, n_ResultLength);
 	sprintf(cstr_ResultText, "%s%d", this->cstr_Text, other);
-#ifdef STRING_DEBUG
-	sprintf(cstr_DebugMessage, "%s:cstr_ResultText = %s\n", __func__, cstr_ResultText);
-	OutputDebugString(cstr_DebugMessage);
-#endif
 	delete[] this->cstr_Text;
 	this->cstr_Text = new char[n_ResultLength];
 	memcpy(this->cstr_Text, cstr_ResultText, n_ResultLength);
@@ -77,23 +224,10 @@ String& String::operator+(short other)
 
 String& String::operator+(int other)
 {
-#ifdef STRING_DEBUG
-	char cstr_DebugMessage[512];
-	sprintf(cstr_DebugMessage, "%s started.\n", __func__);
-	OutputDebugString(cstr_DebugMessage);
-#endif
 	int n_ResultLength = (strlen(this->cstr_Text) + 64 + 1) * sizeof(char);
-#ifdef STRING_DEBUG
-	sprintf(cstr_DebugMessage, "%s:n_ResultLength = %d\n", __func__, n_ResultLength);
-	OutputDebugString(cstr_DebugMessage);
-#endif
 	char* cstr_ResultText = new char[n_ResultLength];
 	memset(cstr_ResultText, 0, n_ResultLength);
 	sprintf(cstr_ResultText, "%s%d", this->cstr_Text, other);
-#ifdef STRING_DEBUG
-	sprintf(cstr_DebugMessage, "%s:cstr_ResultText = %s\n", __func__, cstr_ResultText);
-	OutputDebugString(cstr_DebugMessage);
-#endif
 	delete[] this->cstr_Text;
 	this->cstr_Text = new char[n_ResultLength];
 	memcpy(this->cstr_Text, cstr_ResultText, n_ResultLength);
@@ -103,23 +237,10 @@ String& String::operator+(int other)
 
 String& String::operator+(long other)
 {
-#ifdef STRING_DEBUG
-	char cstr_DebugMessage[512];
-	sprintf(cstr_DebugMessage, "%s started.\n", __func__);
-	OutputDebugString(cstr_DebugMessage);
-#endif
 	int n_ResultLength = (strlen(this->cstr_Text) + 64 + 1) * sizeof(char);
-#ifdef STRING_DEBUG
-	sprintf(cstr_DebugMessage, "%s:n_ResultLength = %d\n", __func__, n_ResultLength);
-	OutputDebugString(cstr_DebugMessage);
-#endif
 	char* cstr_ResultText = new char[n_ResultLength];
 	memset(cstr_ResultText, 0, n_ResultLength);
 	sprintf(cstr_ResultText, "%s%d", this->cstr_Text, other);
-#ifdef STRING_DEBUG
-	sprintf(cstr_DebugMessage, "%s:cstr_ResultText = %s\n", __func__, cstr_ResultText);
-	OutputDebugString(cstr_DebugMessage);
-#endif
 	delete[] this->cstr_Text;
 	this->cstr_Text = new char[n_ResultLength];
 	memcpy(this->cstr_Text, cstr_ResultText, n_ResultLength);
@@ -129,23 +250,10 @@ String& String::operator+(long other)
 
 String& String::operator+(float other)
 {
-#ifdef STRING_DEBUG
-	char cstr_DebugMessage[512];
-	sprintf(cstr_DebugMessage, "%s started.\n", __func__);
-	OutputDebugString(cstr_DebugMessage);
-#endif
 	int n_ResultLength = (strlen(this->cstr_Text) + 64 + 1) * sizeof(char);
-#ifdef STRING_DEBUG
-	sprintf(cstr_DebugMessage, "%s:n_ResultLength = %d\n", __func__, n_ResultLength);
-	OutputDebugString(cstr_DebugMessage);
-#endif
 	char* cstr_ResultText = new char[n_ResultLength];
 	memset(cstr_ResultText, 0, n_ResultLength);
 	sprintf(cstr_ResultText, "%s%f", this->cstr_Text, other);
-#ifdef STRING_DEBUG
-	sprintf(cstr_DebugMessage, "%s:cstr_ResultText = %s\n", __func__, cstr_ResultText);
-	OutputDebugString(cstr_DebugMessage);
-#endif
 	delete[] this->cstr_Text;
 	this->cstr_Text = new char[n_ResultLength];
 	memcpy(this->cstr_Text, cstr_ResultText, n_ResultLength);
@@ -155,23 +263,10 @@ String& String::operator+(float other)
 
 String& String::operator+(double other)
 {
-#ifdef STRING_DEBUG
-	char cstr_DebugMessage[512];
-	sprintf(cstr_DebugMessage, "%s started.\n", __func__);
-	OutputDebugString(cstr_DebugMessage);
-#endif
 	int n_ResultLength = (strlen(this->cstr_Text) + 64 + 1) * sizeof(char);
-#ifdef STRING_DEBUG
-	sprintf(cstr_DebugMessage, "%s:n_ResultLength = %d\n", __func__, n_ResultLength);
-	OutputDebugString(cstr_DebugMessage);
-#endif
 	char* cstr_ResultText = new char[n_ResultLength];
 	memset(cstr_ResultText, 0, n_ResultLength);
 	sprintf(cstr_ResultText, "%s%f", this->cstr_Text, other);
-#ifdef STRING_DEBUG
-	sprintf(cstr_DebugMessage, "%s:cstr_ResultText = %s\n", __func__, cstr_ResultText);
-	OutputDebugString(cstr_DebugMessage);
-#endif
 	delete[] this->cstr_Text;
 	this->cstr_Text = new char[n_ResultLength];
 	memcpy(this->cstr_Text, cstr_ResultText, n_ResultLength);
@@ -191,23 +286,13 @@ String& String::operator+(char other)
 }
 String& String::operator+(char* other)
 {
-#ifdef STRING_DEBUG
-	char cstr_DebugMessage[512];
-	sprintf(cstr_DebugMessage, "%s started.\n", __func__);
-	OutputDebugString(cstr_DebugMessage);
-#endif
+	if (other == nullptr) {
+		throw new Exceptions::NullPointerException();
+	}
 	int n_ResultLength = (strlen(this->cstr_Text) + strlen(other) + 1) * sizeof(char);
-#ifdef STRING_DEBUG
-	sprintf(cstr_DebugMessage, "%s:n_ResultLength = %d\n", __func__, n_ResultLength);
-	OutputDebugString(cstr_DebugMessage);
-#endif
 	char* cstr_ResultText = new char[n_ResultLength];
 	memset(cstr_ResultText, 0, n_ResultLength);
 	sprintf(cstr_ResultText, "%s%s", this->cstr_Text, other);
-#ifdef STRING_DEBUG
-	sprintf(cstr_DebugMessage, "%s:cstr_ResultText = %s\n", __func__, cstr_ResultText);
-	OutputDebugString(cstr_DebugMessage);
-#endif
 	delete[] this->cstr_Text;
 	this->cstr_Text = new char[n_ResultLength];
 	memcpy(this->cstr_Text, cstr_ResultText, n_ResultLength);
@@ -216,50 +301,44 @@ String& String::operator+(char* other)
 }
 String& String::operator+(const char* other)
 {
-#ifdef STRING_DEBUG
-	char cstr_DebugMessage[512];
-	sprintf(cstr_DebugMessage, "%s started.\n", __func__);
-	OutputDebugString(cstr_DebugMessage);
-#endif
+	if (other == nullptr) {
+		throw new Exceptions::NullPointerException();
+	}
 	int n_ResultLength = (strlen(this->cstr_Text) + strlen(other) + 1) * sizeof(char);
-#ifdef STRING_DEBUG
-	sprintf(cstr_DebugMessage, "%s:n_ResultLength = %d\n", __func__, n_ResultLength);
-	OutputDebugString(cstr_DebugMessage);
-#endif
 	char* cstr_ResultText = new char[n_ResultLength];
 	memset(cstr_ResultText, 0, n_ResultLength);
 	sprintf(cstr_ResultText, "%s%s", this->cstr_Text, other);
-#ifdef STRING_DEBUG
-	sprintf(cstr_DebugMessage, "%s:cstr_ResultText = %s\n", __func__, cstr_ResultText);
-	OutputDebugString(cstr_DebugMessage);
-#endif
 	delete[] this->cstr_Text;
 	this->cstr_Text = new char[n_ResultLength];
 	memcpy(this->cstr_Text, cstr_ResultText, n_ResultLength);
 	delete[] cstr_ResultText;
 	return *this;
 }
-/*
+
 String& String::operator+(unsigned short other)
 {
 	// TODO: insert return statement here
+	return *this;
 }
 
 String& String::operator+(unsigned int other)
 {
 	// TODO: insert return statement here
+	return *this;
 }
 
 String& String::operator+(unsigned long other)
 {
 	// TODO: insert return statement here
+	return *this;
 }
 
 String& String::operator+(unsigned char other)
 {
 	// TODO: insert return statement here
+	return *this;
 }
-*/
+
 String& String::operator+(void* other)
 {
 	int n_ResultLength = (strlen(this->cstr_Text) + 64 + 1) * sizeof(char);
@@ -271,12 +350,78 @@ String& String::operator+(void* other)
 	return *this;
 }
 
+String& String::operator+=(String other)
+{
+	return *this + other;
+}
+
+String& String::operator+=(short other)
+{
+	return *this + other;
+}
+
+String& String::operator+=(int other)
+{
+	return *this + other;
+}
+
+String& String::operator+=(long other)
+{
+	return *this + other;
+}
+
+String& String::operator+=(float other)
+{
+	return *this + other;
+}
+
+String& String::operator+=(double other)
+{
+	return *this + other;
+}
+
+String& String::operator+=(char other)
+{
+	return *this + other;
+}
+
+String& String::operator+=(char* other)
+{
+	if (other == nullptr) {
+		throw new Exceptions::NullPointerException();
+	}
+	return *this + other;
+}
+
+String& String::operator+=(const char* other)
+{
+	if (other == nullptr) {
+		throw new Exceptions::NullPointerException();
+	}
+	return *this + other;
+}
+
+String& String::operator+=(unsigned short other)
+{
+	return *this + other;
+}
+
+String& String::operator+=(unsigned int other)
+{
+	return *this + other;
+}
+
+String& String::operator+=(unsigned long other)
+{
+	return *this + other;
+}
+
+String& String::operator+=(unsigned char other)
+{
+	return *this + other;
+}
+
 String::operator char* ()
 {
-#ifdef STRING_DEBUG
-	char cstr_DebugMessage[512];
-	sprintf(cstr_DebugMessage, "String::operator char* started.\n");
-	OutputDebugString(cstr_DebugMessage);
-#endif
 	return this->cstr_Text;
 }
--- SAMP Server Template2/String.h
+++ SAMP Server Template2/String.h
@@ -2,7 +2,22 @@
 #define STRING_H
 
 #ifdef _DEBUG
-//#define STRING_DEBUG
+#define STRING_DEBUG
+
+#ifdef STRING_DEBUG
+
+#define DEBUG_START(message_Size)\
+					char cstr_DebugMessage[message_Size];\
+					char cstr_FormatString[message_Size];\
+					memset(cstr_DebugMessage, 0, (message_Size)*sizeof(char));\
+					sprintf(cstr_DebugMessage, "%s::Debug started.\n", __func__);\
+					OutputDebugString(cstr_DebugMessage);
+
+#define DEBUG_MESSAGE(format, ...)\
+					sprintf(cstr_FormatString, "%s::%s",__func__, format);\
+					sprintf(cstr_DebugMessage, cstr_FormatString, __func__, __VA_ARGS__);\
+					OutputDebugString(cstr_DebugMessage);
+#endif
 #endif
 
 
@@ -16,8 +31,33 @@ public:
 	String(char* cstr_text);
 	~String();
 
+	void TrimLeft(int n_Characters);
+	void TrimRight(int n_Characters);
+	void PadLeft(int n_Characters, char c_PadCharacter = ' ');
+	void PadRight(int n_Characters, char c_PadCharacter = ' ');
+
+	Array<String> Split(const char c_Delimiter);
+	Array<String> Split(const char* c_Delimiters, int n_DelimiterCount);
+
+	void Replace(String what, String with);
 
 
+	/*
+	String& operator =(String other);
+	String& operator =(short other);
+	String& operator =(int other);
+	String& operator =(long other);
+	String& operator =(float other);
+	String& operator =(double other);
+	String& operator =(char other);
+	String& operator =(char* other);
+	String& operator =(const char* other);
+
+	String& operator =(unsigned short other);
+	String& operator =(unsigned int other);
+	String& operator =(unsigned long other);
+	String& operator =(unsigned char other);
+	*/
 
 	String& operator +(String other);
 	String& operator +(short other);
@@ -35,6 +75,23 @@ public:
 	String& operator +(unsigned char other);
 
 	String& operator +(void* other);
+
+	String& operator +=(String other);
+	String& operator +=(short other);
+	String& operator +=(int other);
+	String& operator +=(long other);
+	String& operator +=(float other);
+	String& operator +=(double other);
+	String& operator +=(char other);
+	String& operator +=(char* other);
+	String& operator +=(const char* other);
+
+	String& operator +=(unsigned short other);
+	String& operator +=(unsigned int other);
+	String& operator +=(unsigned long other);
+	String& operator +=(unsigned char other);
+
+	
 
 	operator char* ();
 };
