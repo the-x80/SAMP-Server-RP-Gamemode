0e9526d7b38ba3d595d4d27d2e4c66195f7034a5
the-x80 <76942844+the-x80@users.noreply.github.com>
2021-09-11 12:48:16 +0200

    Introduced File Searching

--- SAMP Server Template2/IO.cpp
+++ SAMP Server Template2/IO.cpp
@@ -1,6 +1,154 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include "IO.h"
+
+
+
+#ifdef _DEBUG
+#define DEBUG_INITIALIZE
+
+
+
+
+#define DEBUG_START												\
+				char* cstr_DebugMessage = new char[2048];		\
+				memset(cstr_DebugMessage, 0, 2048*sizeof(char));
+
+
+#define DEBUG_MESSAGE(msg, ...)												\
+				memset(cstr_DebugMessage, 0, sizeof(char)*2048);			\										\
+				sprintf_s(cstr_DebugMessage, 2048 * sizeof(char), "%s::%s", __func__, msg);		\
+				{															\
+					char* cstr_FormatedMessage = new char[2048];			\
+					sprintf_s(cstr_FormatedMessage, 2048*sizeof(char), cstr_DebugMessage, __VA_ARGS__);\
+					OutputDebugString(cstr_FormatedMessage);				\
+					delete[] cstr_FormatedMessage;							\
+				}
+
+
+#define DEBUG_END							\
+				delete[] cstr_DebugMessage;	\
+				cstr_DebugMessage = nullptr;
+
+
+#define DIAGNOSTICS_START\
+			LARGE_INTEGER li_qpcStart;\
+			QueryPerformanceCounter(&li_qpcStart);
+
+#define DIAGNOSTICS_END\
+			LARGE_INTEGER li_qpcEnd;\
+			LARGE_INTEGER li_qpcFreq;\
+			QueryPerformanceCounter(&li_qpcEnd);\
+			QueryPerformanceFrequency(&li_qpcFreq);\
+			DEBUG_MESSAGE("Execution time:%f\nQPCStart %u\nQPCEnd %u\n", (float)((li_qpcEnd.LowPart - li_qpcStart.LowPart) / li_qpcFreq.LowPart), li_qpcStart.LowPart, li_qpcEnd.LowPart);\
+			DEBUG_MESSAGE("Tick count %u\n", li_qpcEnd.LowPart - li_qpcStart.LowPart);
+#endif
+
+
+
+
+
+
+
 
 int IO::GetLogicalDriveCount()
 {
 	return 0;
+}
+
+bool IO::SearchFolderForFile(char* cstr_Filename, char* cstr_FolderPath, char* cstr_FullPath, int* cstr_FullPathLength, bool b_SearchSubdirectories)
+{
+#pragma region Sanity checks
+	if (cstr_Filename == nullptr) {
+		throw new ::Exceptions::ArgumentNullException();
+	}
+	if (cstr_FolderPath == nullptr) {
+		throw new ::Exceptions::ArgumentNullException();
+	}
+	if (cstr_FullPath == nullptr) {
+		throw new ::Exceptions::ArgumentNullException();
+	}
+
+	//Check if the csrt_FolderPath ends with a trailing slash
+	//If not add the trailing slash to the string.
+	char cstr_SearchPath[MAX_PATH*2] = "";
+#ifndef _CRT_SECURE_NO_WARNINGS
+	if (cstr_FolderPath[strlen(cstr_FolderPath) - 1] != '\\') {
+		sprintf_s(cstr_SearchPath, sizeof(char) * MAX_PATH * 2, "%s\\*", cstr_FolderPath);
+	}
+	else {
+		sprintf_s(cstr_SearchPath, sizeof(char) * MAX_PATH * 2, "%s*", cstr_FolderPath);
+	}
+#else
+	if (cstr_FolderPath[strlen(cstr_FolderPath) - 1] != '\\') {
+		sprintf(cstr_SearchPath, "%s\\*", cstr_FolderPath);
+	}
+	else {
+		sprintf(cstr_SearchPath, "%s*", cstr_FolderPath);
+	}
+#endif
+#pragma endregion
+
+	WIN32_FIND_DATA fd_FindData = { 0 };
+	HANDLE h_FindFile = FindFirstFile(cstr_SearchPath, &fd_FindData);
+	if (h_FindFile == INVALID_HANDLE_VALUE) {
+		//Handle the error.
+	}
+
+	bool b_RetVal = false;
+	do {
+		if (strcmp(cstr_Filename, fd_FindData.cFileName) == 0) {
+			b_RetVal = true;
+
+			int n_RequiredFullPathLength = strlen(cstr_SearchPath) + strlen(fd_FindData.cFileName);
+			if (n_RequiredFullPathLength > *cstr_FullPathLength) {
+				throw new ::Exceptions::OutOfMemoryException();
+			}
+
+			break;
+		}
+		else {
+			if ((fd_FindData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) && (b_SearchSubdirectories)) {
+				if (strcmp(fd_FindData.cFileName, ".") == 0) {
+					continue;
+				}
+				if (strcmp(fd_FindData.cFileName, "..") == 0) {
+					continue;
+				}
+
+				char cstr_SubFolderPath[MAX_PATH*2] = "";
+#ifndef _CRT_SECURE_NO_WARNINGS
+				sprintf_s(cstr_SubFolderPath, sizeof(char) * MAX_PATH*2, "%s%s\\", cstr_FolderPath, fd_FindData.cFileName);
+#else
+				sprintf(cstr_SubFolderPath, "%s%s\\", cstr_FolderPath, fd_FindData.cFileName);
+#endif
+				b_RetVal = FALSE;
+				try {
+					b_RetVal = SearchFolderForFile(cstr_Filename, cstr_SubFolderPath, cstr_FullPath, cstr_FullPathLength, b_SearchSubdirectories);
+				}
+				catch (::Exceptions::ArgumentNullException* e) {
+					b_RetVal = FALSE;
+				}
+				catch (::Exceptions::OutOfMemoryException* e) {
+					b_RetVal = FALSE;
+				}
+				catch (::Exceptions::Exception* e) {
+					b_RetVal = FALSE;
+				}
+				if (b_RetVal == true) {
+					break;
+				}
+			}
+		}
+	} while (FindNextFile(h_FindFile, &fd_FindData));
+	BOOL b_FindCloseResult = FindClose(h_FindFile);
+	if (b_FindCloseResult == FALSE) {
+		//Handle the error.
+	}
+
+	return b_RetVal;
+}
+
+bool IO::SearchDriveForFile(char* cstr_Filename, char* cstr_DriveLetter, char* cstr_FullPath)
+{
+	return false;
 }
--- SAMP Server Template2/IO.h
+++ SAMP Server Template2/IO.h
@@ -25,6 +25,25 @@ namespace IO {
 	/// <exception cref="IO::Exceptions::IOException">Thrown when an unknown io exception occurs</exception>
 	int GetLogicalDriveCount();
 
+	/// <summary>
+	/// Searches for a file or directory inside a directory
+	/// </summary>
+	/// <param name="cstr_Filename">The file or folder name used to perform the search</param>
+	/// <param name="cstr_FolderPath">The folders path where to search.</param>
+	/// <param name="cstr_FullPath">The string containing the result of the search</param>
+	/// <param name="cstr_FullPathLength">The length of the string containing the result of the search</param>
+	/// <param name="b_SearchSubdirectories">If set to true subdirectories are searched aswell.</param>
+	/// <returns>True if the file has been found otherwise false.</returns>
+	/// <remarks>
+	/// This function is very slow since it has to access the hard drive for each file.
+	/// There could be a posibility to speed up the process by multithreading.
+	/// </remarks>
+	/// <exception cref="::Exceptions::ArgumentNullException"></exception>
+	/// <exception cref="::Exceptions::OutOfMemoryException"></exception>
+	/// <exception cref="::IO::Exceptions::IOException"></exception>
+	bool SearchFolderForFile(char* cstr_Filename, char* cstr_FolderPath, char* cstr_FullPath, int* cstr_FullPathLength, bool b_SearchSubdirectories);
+	bool SearchDriveForFile(char* cstr_Filename, char* cstr_DriveLetter, char* cstr_FullPath);
+	bool SearchSystemForFile(char* cstr_Filename, char* cstr_FullPath);
 
 	//int EnumerateLogicalDrives(); Unused for now
 }
